5bd34704c994b59179382b0629da298f3de70f31
Hake Huang <hake.huang@nxp.com>
2016-08-04 19:01:49 +0800

    update enc pha phb phase
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

--- ips/ip/enc/enc.v
+++ ips/ip/enc/enc.v
@@ -21,23 +21,23 @@ reg led;
 
 // generate 100 Hz from 50 MHz
 reg [31:0] count_reg;
-reg out_100hz;
+reg out_200hz;
 
 always @(posedge freq_clk or negedge rst_n) begin
     if (!rst_n) begin
         count_reg <= 0;
-        out_100hz <= 0;
+        out_200hz <= 0;
         count_reg <= 0;
-        out_100hz <= 0;
+        out_200hz <= 0;
 	
     end 
 	 else if (enable) 
 	 begin
-        if (count_reg < 249999) begin
+        if (count_reg < 124999) begin
             count_reg <= count_reg + 1;
         end else begin
             count_reg <= 0;
-            out_100hz <= ~out_100hz;
+            out_200hz <= ~out_200hz;
         end
     end
 end
@@ -46,8 +46,8 @@ end
   we will be generating waveform like below
         _   _
   pha  | |_| |_
-          _   _  
-  phb  __| |_| |_
+         _   _  
+  phb  _| |_| |_
                                  _
   home <every 12 clock of pha> _| |_
                                   _
@@ -55,54 +55,53 @@ end
 */
 
 /* process the  pha_count*/
-always @ (posedge out_100hz or negedge rst_n)
+always @ (posedge out_200hz or negedge rst_n)
 begin
 	if (!rst_n)
 	begin
-		pha_count <= 8'd12;
+		pha_count <= 8'd0;
 		led <= 1'b0; 
 	end
-	else if (out_100hz)
+	else if (out_200hz)
 	begin
 	   led <= ~led; 
-		case (pha_count)
-			8'd1:	pha_count <= 8'd2;
-			8'd2:	pha_count <= 8'd3;
-			8'd3:	pha_count <= 8'd4;
-			8'd4: pha_count <= 8'd5;
-			8'd5: pha_count <= 8'd6;
-			8'd6: pha_count <= 8'd7;
-			8'd7: pha_count <= 8'd8;
-			8'd8:	pha_count <= 8'd9;
-			8'd9: pha_count <= 8'd10;
-			8'd10: pha_count <= 8'd11;
-			8'd11: pha_count <= 8'd12;
-			8'd12: pha_count <= 8'd0;
-			8'd0: pha_count <=  8'd1;
-		endcase
+		if(pha_count>8'd24)
+			pha_count <= 8'd0;
+		else
+			pha_count <= pha_count + 8'd1;
 	end
 end
 
-
+reg[1:0] Phase90_Count;
 /*process the pha signal*/
-always @ (posedge out_100hz or negedge rst_n)
+always @ (posedge out_200hz or negedge rst_n)
 begin
 	if (!rst_n)
 	begin
-		pha_reg <= 1'b1;
+		Phase90_Count <= 2'b0;
 	end
-	else if (out_100hz)
+	else if (out_200hz)
 	begin
-		case (pha_reg)
-			1'd1:	
+		case (Phase90_Count)
+			2'd0:	
 			begin
-			pha_reg <=  1'd0;
-			phb_reg <= 1'd1;
+                pha_reg <=  1'd1;
+                Phase90_Count <= Phase90_Count + 2'd1;
 			end
-			1'd0: 
+			2'd1: 
 			begin
-			pha_reg <=  1'd1;
-			phb_reg <=  1'd0;
+                phb_reg <=  1'd1;
+                Phase90_Count <= Phase90_Count + 2'd1;
+			end		 
+			2'd2:	
+			begin
+                pha_reg <=  1'd0;
+                Phase90_Count <= Phase90_Count + 2'd1;
+			end
+			2'd3: 
+			begin
+                phb_reg <=  1'd0;
+                Phase90_Count <= 2'd0;
 			end
 		endcase
 	end
@@ -111,16 +110,17 @@ assign pha = pha_reg;
 assign phb = phb_reg;
 
 /*process the home signal*/
-always @ (posedge out_100hz or negedge rst_n)
+always @ (posedge out_200hz or negedge rst_n)
 begin
 	if (!rst_n)
 	begin
 		home_reg <= 1'b0;
 	end
-	else if (out_100hz)
+	else if (out_200hz)
 	begin
 		case (pha_count)
-			8'd12:	home_reg <=  1'd1;
+            8'd23:	home_reg <=  1'd1;
+			8'd24:	home_reg <=  1'd1;
 			default: home_reg <=  1'd0;
 		endcase
 	end
@@ -128,16 +128,17 @@ end
 assign home = home_reg;
 
 /*process the index signal*/
-always @ (posedge out_100hz or negedge rst_n)
+always @ (posedge out_200hz or negedge rst_n)
 begin
 	if (!rst_n)
 	begin
 		index_reg <= 1'b0;
 	end
-	else if (out_100hz)
+	else if (out_200hz)
 	begin
 		case (pha_count)
-			8'd12:	index_reg <=  1'd1;
+			8'd23:	index_reg <=  1'd1;
+			8'd24:	index_reg <=  1'd1;
 			default: index_reg <=  1'd0;
 		endcase
 	end
